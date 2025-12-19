fee6c576da765da50b9aeaef6be08cabd1b34116
Muneeb1024 <muneeblodhi124@gmail.com>
2025-12-20 01:56:22 +0500

    commit

--- physical-ai-book/docusaurus.config.js
+++ physical-ai-book/docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Muneeb1024/physical-ai-humanoid-textbook-1.0',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
