941dbf95c3490c9f576c90ce7afb11a0a174bd43
Muneeb1024 <muneeblodhi124@gmail.com>
2025-12-20 01:40:25 +0500

    update

--- physical-ai-book/docusaurus.config.js
+++ physical-ai-book/docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://github.com/Muneeb1024/physical-ai-humanoid-textbook-1.0',
+  url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Muneeb1024/physical-ai-humanoid-textbook-1.0',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
